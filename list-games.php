9e3c7cb155eea73f3f8efeffbd95e6fa0ca9d9a2
Danial Chughtai <2441631@csl-student.wlv.ac.uk>
2025-10-19 05:49:21 +0100

    Added the game adding form

--- list-games.php
+++ list-games.php
@@ -9,11 +9,16 @@ include("db.php");
 $sql = "SELECT * FROM videogames ORDER BY released_date";
 $results = mysqli_query($mysqli, $sql);
 ?>
-
+<div class="head">
+    
 <form action="search-games.php" method="post">
 <input type="text" name="keywords" placeholder="Search">
 <input type="submit" value="Go!">
 </form>
+
+<a href="add-game-form.php" class="btn btn-primary">Add a game</a>
+
+</div>
 
 <table>
 <?php while($a_row = mysqli_fetch_assoc($results)):?>
