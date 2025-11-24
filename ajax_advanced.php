e899817c2131433f5ce881382cb8ff0daefc8db0
Danial Chughtai <2441631@csl-student.wlv.ac.uk>
2025-11-24 14:19:35 +0000

    fixed the search cards,made tehm clickable
    ain

---
+++ ajax_advanced.php
@@ -0,0 +1,28 @@
+<?php
+include("db.php");
+
+$search = $_GET['search'];
+$maxPrice = $_GET['maxPrice'];
+$years = $_GET['years'];
+$conditions = $_GET['conditions'];
+$transmissions = $_GET['transmissions'];
+
+$sql = "SELECT * FROM devincars WHERE price <= $maxPrice";
+
+if ($search != '')
+$sql .= " AND car_model LIKE '%$search%'";
+
+if ($years != '')
+$sql .= " AND car_year IN ($years)";
+
+if ($conditions != '')
+$sql .= " AND `condition` IN ('$conditions')";
+
+if ($transmissions != '')
+$sql .= " AND transmission IN ('$transmissions')";
+
+$sql .= " ORDER BY price";
+
+$results = $mysqli->query($sql)->fetch_all(MYSQLI_ASSOC);
+print(json_encode($results));
+?>
