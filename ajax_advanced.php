e528fb89b9667862141920c26bf83651ceb937b8
Danial Chughtai <2441631@csl-student.wlv.ac.uk>
2025-11-28 04:02:56 +0000

    Fixed security error on advanced search page
    ain

--- ajax_advanced.php
+++ ajax_advanced.php
@@ -9,17 +9,29 @@ $transmissions = $_GET['transmissions'];
 
 $sql = "SELECT * FROM devincars WHERE price <= $maxPrice";
 
-if ($search != '')
-$sql .= " AND car_model LIKE '%$search%'";
+// if ($search != '')
+// $sql .= " AND car_model LIKE '%$search%'";
 
-if ($years != '')
-$sql .= " AND car_year IN ($years)";
+// if ($years != '')
+// $sql .= " AND car_year IN ($years)";
 
-if ($conditions != '')
-$sql .= " AND `condition` IN ('$conditions')";
+// if ($conditions != '')
+// $sql .= " AND `condition` IN ('$conditions')";
 
-if ($transmissions != '')
-$sql .= " AND transmission IN ('$transmissions')";
+// if ($transmissions != '')
+// $sql .= " AND transmission IN ('$transmissions')";
+if ($search !== '')
+    $sql .= " AND car_model LIKE '%$search%'";
+
+if ($years !== '' && preg_match('/^[0-9,]+$/', $years))
+    $sql .= " AND car_year IN ($years)";
+
+if ($conditions !== '' && preg_match('/^[a-zA-Z, ]+$/', $conditions))
+    $sql .= " AND `condition` IN ('$conditions')";
+
+if ($transmissions !== '' && preg_match('/^[a-zA-Z, ]+$/', $transmissions))
+    $sql .= " AND transmission IN ('$transmissions')";
+
 
 $sql .= " ORDER BY price";
 
